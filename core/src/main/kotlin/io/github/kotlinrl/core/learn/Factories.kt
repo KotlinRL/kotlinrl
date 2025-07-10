51365883469aa37b5a0adaca6a4cd29fa572e2dc
Daniel Krieg <daniel_krieg@mac.com>
2025-07-10 15:41:45 -0500

    Update `mutableQFunction` to return a pair of state-action value table and Q-function

--- core/src/main/kotlin/io/github/kotlinrl/core/learn/Factories.kt
+++ core/src/main/kotlin/io/github/kotlinrl/core/learn/Factories.kt
@@ -8,14 +8,17 @@ import org.jetbrains.kotlinx.multik.ndarray.data.get
 fun <State, Action> mutableQFunction(
     shape: IntArray,
     indexFor: (State, Action) -> IntArray
-): MutableQFunction<State, Action> = object : MutableQFunction<State, Action> {
+): Pair<NDArray<Double, DN>, MutableQFunction<State, Action>> {
     val table = mk.zeros<Double, DN>(shape, DoubleDataType)
 
-    override fun update(state: State, action: Action, newValue: Double) {
-        table[indexFor(state, action)] = newValue
-    }
+    return table to object : MutableQFunction<State, Action> {
 
-    override fun invoke(state: State, action: Action): Double {
-        return table[indexFor(state, action)]
+        override fun update(state: State, action: Action, newValue: Double) {
+            table[indexFor(state, action)] = newValue
+        }
+
+        override fun invoke(state: State, action: Action): Double {
+            return table[indexFor(state, action)]
+        }
     }
 }
