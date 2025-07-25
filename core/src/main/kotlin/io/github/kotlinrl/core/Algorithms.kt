98b7de5f3959fe43e857978f7672eaca36facd95
Daniel Krieg <daniel_krieg@mac.com>
2025-07-25 01:09:15 -0500

    Add `constantAlphaMonteCarloControlAgent` and streamline value iteration and policy iteration implementations for improved modularity and consistency in agent creation.

--- core/src/main/kotlin/io/github/kotlinrl/core/Algorithms.kt
+++ core/src/main/kotlin/io/github/kotlinrl/core/Algorithms.kt
@@ -1,7 +1,7 @@
 package io.github.kotlinrl.core
 
-import io.github.kotlinrl.core.algorithms.mc.defaultKeyFunction
-import kotlin.random.Random
+import io.github.kotlinrl.core.algorithms.mc.*
+import kotlin.random.*
 
 typealias QFunction<State, Action> = io.github.kotlinrl.core.policy.QFunction<State, Action>
 typealias QTable = io.github.kotlinrl.core.algorithms.QTable
@@ -19,70 +19,27 @@ typealias SARSA<State, Action> = io.github.kotlinrl.core.algorithms.td.SARSA<Sta
 typealias NStepSARSA<State, Action> = io.github.kotlinrl.core.algorithms.td.nstep.NStepSARSA<State, Action>
 typealias TabularTDLearning<State, Action> = io.github.kotlinrl.core.algorithms.td.TabularTDLearning<State, Action>
 
-fun <State, Action> qLearning(
-    qTable: QFunction<State, Action>,
-    alpha: ParameterSchedule,
-    gamma: Double
-): ObserveTransition<State, Action> = QLearning(qTable, alpha, gamma)
-
-fun <State, Action> sarsa(
-    qTable: QFunction<State, Action>,
-    alpha: ParameterSchedule,
-    gamma: Double
-): ObserveTransition<State, Action> = SARSA(qTable, alpha, gamma)
-
-fun <State, Action> expectedSARSA(
-    qTable: QFunction<State, Action>,
-    alpha: ParameterSchedule,
-    gamma: Double,
-    stateActionListProvider: StateActionListProvider<State, Action>,
-    policyProbabilities: PolicyProbabilities<State, Action>
-): ObserveTransition<State, Action> = ExpectedSARSA(
-    qTable = qTable,
-    alpha = alpha,
-    gamma = gamma,
-    stateActionListProvider = stateActionListProvider,
-    policyProbabilities = policyProbabilities
-)
-
-fun <State, Action> nStepSARSA(
-    qTable: QFunction<State, Action>,
-    alpha: ParameterSchedule,
-    gamma: Double,
-    n: Int,
-    policyProbabilities: PolicyProbabilities<State, Action>
-): NStepSARSA<State, Action> = NStepSARSA(
-    qTable = qTable,
-    alpha = alpha,
-    gamma = gamma,
-    n = n,
-    policyProbabilities = policyProbabilities
-)
-
 fun <State, Action> valueIteration(
     gamma: Double = 0.99,
     theta: Double = 1e-6,
     vTable: ValueFunction<State>,
     pTable: MutablePolicy<State, Action>,
-    env: ModelBasedEnv<State, Action, *, *>,
     stateActionListProvider: StateActionListProvider<State, Action>,
-    actionComparator: Comparator<Action>
-): Policy<State, Action> = valueIterationPlanner(gamma, theta, vTable, pTable, actionComparator)
-    .plan(
-        stateActionListProvider = stateActionListProvider,
-        transitionFunction = { state, action ->
-            val stepResult: StepResult<State> = env.simulateStep(state, action)
-            Transition(
-                state = state,
-                action = action,
-                reward = stepResult.reward,
-                nextState = stepResult.state,
-                terminated = stepResult.terminated,
-                truncated = stepResult.truncated,
-                info = stepResult.info
-            )
-        }
-    )
+    actionComparator: Comparator<Action>,
+    env: ModelBasedEnv<State, Action, *, *>
+): Policy<State, Action> =
+    valueIteration(gamma, theta, vTable, pTable, stateActionListProvider, actionComparator) { state, action ->
+        val stepResult: StepResult<State> = env.simulateStep(state, action)
+        Transition(
+            state = state,
+            action = action,
+            reward = stepResult.reward,
+            nextState = stepResult.state,
+            terminated = stepResult.terminated,
+            truncated = stepResult.truncated,
+            info = stepResult.info
+        )
+    }
 
 fun <State, Action> valueIteration(
     gamma: Double = 0.99,
@@ -90,37 +47,45 @@ fun <State, Action> valueIteration(
     vTable: ValueFunction<State>,
     pTable: MutablePolicy<State, Action>,
     stateActionListProvider: StateActionListProvider<State, Action>,
-    transitionFunction: TransitionFunction<State, Action>,
-    actionComparator: Comparator<Action>
+    actionComparator: Comparator<Action>,
+    transitionFunction: TransitionFunction<State, Action>
 ): Policy<State, Action> = valueIterationPlanner(gamma, theta, vTable, pTable, actionComparator)
     .plan(
         stateActionListProvider = stateActionListProvider,
         transitionFunction = transitionFunction
     )
 
+fun <State, Action> policyIteration(
+    gamma: Double = 0.99,
+    theta: Double = 1e-6,
+    vTable: ValueFunction<State>,
+    pTable: MutablePolicy<State, Action>,
+    stateActionListProvider: StateActionListProvider<State, Action>,
+    env: ModelBasedEnv<State, Action, *, *>
+): Policy<State, Action> = policyIteration(gamma, theta, vTable, pTable, stateActionListProvider) { state, action ->
+    val stepResult: StepResult<State> = env.simulateStep(state, action)
+    Transition(
+        state = state,
+        action = action,
+        reward = stepResult.reward,
+        nextState = stepResult.state,
+        terminated = stepResult.terminated,
+        truncated = stepResult.truncated,
+        info = stepResult.info
+    )
+}
 
 fun <State, Action> policyIteration(
     gamma: Double = 0.99,
     theta: Double = 1e-6,
     vTable: ValueFunction<State>,
     pTable: MutablePolicy<State, Action>,
-    env: ModelBasedEnv<State, Action, *, *>,
-    stateActionListProvider: StateActionListProvider<State, Action>
+    stateActionListProvider: StateActionListProvider<State, Action>,
+    transitionFunction: TransitionFunction<State, Action>
 ): Policy<State, Action> = policyIterationPlanner(gamma, theta, vTable, pTable)
     .plan(
         stateActionListProvider = stateActionListProvider,
-        transitionFunction = { state, action ->
-            val stepResult: StepResult<State> = env.simulateStep(state, action)
-            Transition(
-                state = state,
-                action = action,
-                reward = stepResult.reward,
-                nextState = stepResult.state,
-                terminated = stepResult.terminated,
-                truncated = stepResult.truncated,
-                info = stepResult.info
-            )
-        }
+        transitionFunction = transitionFunction
     )
 
 fun <State, Action> onPolicyMonteCarloControl(
@@ -190,3 +155,43 @@ fun <State, Action> epsilonGreedySoftOffPolicyControls(
     )
     return behavior to probability
 }
+
+fun <State, Action> qLearning(
+    qTable: QFunction<State, Action>,
+    alpha: ParameterSchedule,
+    gamma: Double
+): ObserveTransition<State, Action> = QLearning(qTable, alpha, gamma)
+
+fun <State, Action> sarsa(
+    qTable: QFunction<State, Action>,
+    alpha: ParameterSchedule,
+    gamma: Double
+): ObserveTransition<State, Action> = SARSA(qTable, alpha, gamma)
+
+fun <State, Action> expectedSARSA(
+    qTable: QFunction<State, Action>,
+    alpha: ParameterSchedule,
+    gamma: Double,
+    stateActionListProvider: StateActionListProvider<State, Action>,
+    policyProbabilities: PolicyProbabilities<State, Action>
+): ObserveTransition<State, Action> = ExpectedSARSA(
+    qTable = qTable,
+    alpha = alpha,
+    gamma = gamma,
+    stateActionListProvider = stateActionListProvider,
+    policyProbabilities = policyProbabilities
+)
+
+fun <State, Action> nStepSARSA(
+    qTable: QFunction<State, Action>,
+    alpha: ParameterSchedule,
+    gamma: Double,
+    n: Int,
+    policyProbabilities: PolicyProbabilities<State, Action>
+): NStepSARSA<State, Action> = NStepSARSA(
+    qTable = qTable,
+    alpha = alpha,
+    gamma = gamma,
+    n = n,
+    policyProbabilities = policyProbabilities
+)
