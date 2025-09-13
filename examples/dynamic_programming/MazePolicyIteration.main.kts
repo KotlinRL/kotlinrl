6eb6a2ef17a515224d14ae4063cc8a78f3bcff43
Daniel Krieg <daniel_krieg@mac.com>
2025-09-13 17:58:50 -0500

    Add example for policy iteration using Maze environment
    - Introduced `MazePolicyIteration.main.kts` script demonstrating policy iteration with the KotlinRL `Maze` environment.
    - Added Jupyter Notebook `MazePolicyIteration.ipynb` for interactive visualization and step-by-step explanation.
    - Demonstrated policy plotting and video rendering functionalities for the maze environment.

---
+++ examples/dynamic_programming/MazePolicyIteration.main.kts
@@ -0,0 +1,48 @@
+#!/usr/bin/env kotlin -Djavafx.platform=mac-aarch64 -Djava.awt.headless=false -Dprism.order=sw
+
+@file:Repository("https://repo1.maven.org/maven2")
+@file:Repository("https://central.sonatype.com/repository/maven-snapshots/")
+
+@file:DependsOn("io.github.kotlinrl:integration:0.1.0-SNAPSHOT")
+@file:DependsOn("io.github.kotlinrl:tabular:0.1.0-SNAPSHOT")
+@file:DependsOn("io.github.kotlinrl:envs:0.1.0-SNAPSHOT")
+@file:DependsOn("io.github.kotlinrl:rendering:0.1.0-SNAPSHOT")
+@file:DependsOn("org.openjfx:javafx-base:17.0.10")
+@file:DependsOn("org.openjfx:javafx-graphics:17.0.10")
+@file:DependsOn("org.openjfx:javafx-controls:17.0.10")
+@file:DependsOn("org.openjfx:javafx-media:17.0.10")
+
+import io.github.kotlinrl.core.*
+import io.github.kotlinrl.core.wrapper.saveBufferedImageAsPng
+import io.github.kotlinrl.envs.*
+import io.github.kotlinrl.rendering.*
+import io.github.kotlinrl.tabular.*
+import org.jetbrains.kotlinx.kandy.letsplot.export.*
+import java.io.*
+
+
+val testEpisodes = 3
+
+val env = Maze(render = true)
+val recordEnv = RecordVideo(env, folder = "./videos/maze_policy_iteration")
+
+val planner = policyIteration()
+val (policy, vTable) = planner.plan(env.asMDP(0.9))
+
+val trainer = episodicTrainer(
+    agent = policyAgent(policy = policy.pi()),
+    env = recordEnv,
+    successfulTermination = { it.reward == 0.0 },
+    closeOnSuccess = true,
+)
+val testResults = trainer.train(maxEpisodes(testEpisodes))
+println("Test average reward: ${testResults.totalAverageReward}")
+repeat(testEpisodes) {
+    displayVideo(
+        episode = it + 1,
+        folder = recordEnv.folder
+    )
+}
+printPolicyGrid(policy, 5, mapOf(0 to "↑", 1 to "→", 2 to "↓", 3 to "←"))
+val plot = plotPolicyStateValueGrid(policy, vTable, 5, mapOf(0 to "↑", 1 to "→", 2 to "↓", 3 to "←"))
+saveBufferedImageAsPng(plot.toBufferedImage(), File(recordEnv.folder, "policy_grid.png"))
