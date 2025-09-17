eef30a628115a41bcc8ade81364a71f352dcac8c
Daniel Krieg <daniel_krieg@mac.com>
2025-09-17 09:44:57 -0500

    Add Maze On-Policy Monte Carlo Control example notebook
    - Introduces `MazeOnPolicyMonteCarloControl.ipynb` demonstrating training and evaluation using the Maze environment.
    - Includes hyperparameter definition, episodic training process, testing setup, visualization of results (policy, action-value grids), and video recording.

---
+++ examples/monte_carlo_control/MazeOffPolicyMonteCarloControl.main.kts
@@ -0,0 +1,119 @@
+#!/usr/bin/env kotlin -Djava.awt.headless=false -Dprism.order=sw
+
+@file:Repository("https://repo1.maven.org/maven2")
+@file:Repository("https://central.sonatype.com/repository/maven-snapshots/")
+
+@file:DependsOn("io.github.kotlinrl:integration:0.1.0-SNAPSHOT")
+@file:DependsOn("io.github.kotlinrl:tabular:0.1.0-SNAPSHOT")
+@file:DependsOn("io.github.kotlinrl:envs:0.1.0-SNAPSHOT")
+@file:DependsOn("io.github.kotlinrl:rendering:0.1.0-SNAPSHOT")
+
+import io.github.kotlinrl.core.*
+import io.github.kotlinrl.core.RecordVideo
+import io.github.kotlinrl.core.api.*
+import io.github.kotlinrl.core.wrapper.*
+import io.github.kotlinrl.envs.*
+import io.github.kotlinrl.rendering.*
+import io.github.kotlinrl.tabular.*
+import io.github.kotlinrl.tabular.mc.*
+import org.jetbrains.kotlinx.kandy.letsplot.export.*
+import org.jetbrains.kotlinx.multik.api.*
+import org.jetbrains.kotlinx.multik.api.io.*
+import org.jetbrains.kotlinx.multik.ndarray.data.*
+import org.jetbrains.kotlinx.multik.ndarray.operations.max
+import java.io.*
+
+
+val maxStepsPerEpisode = 4_500
+val trainingEpisodes = 1_000
+val testEpisodes = 30
+val initialEpsilon = 0.99
+val linearDecayRate = 0.001
+val minEpsilon = 0.0
+val gamma = 0.99
+val fileName = "MazeOffPolicyMonteCarloControl.npy"
+
+val env = Maze(render = true)
+val recordEnv = RecordVideo(env = env, folder = "./videos/maze_off_policy_mcc", 10)
+
+val policy = mk.d1array(25) { 0 }
+val trainingQtable: QTable = mk.d2array(25, 4) { -10.0 }
+(0 until trainingQtable.shape[1]).forEach { trainingQtable[24, it] = 0.0 }
+val (epsilonSchedule, epsilonDecay) = ParameterSchedule.linearDecay(
+    initialValue = initialEpsilon,
+    minValue = minEpsilon,
+    decayRate = linearDecayRate,
+    callback = { episode, parameter ->
+        if (episode % 100 == 0) {
+            println("Episode: $episode, Epsilon: $parameter")
+        }
+    }
+)
+val trainer = episodicTrainer(
+    env = env,
+    agent = learningAgent(
+        id = "training",
+        algorithm = OffPolicyMonteCarloControl(
+            initialTargetPolicy = policy,
+            epsilon = epsilonSchedule,
+            Q = trainingQtable,
+            gamma = gamma,
+            firstVisitOnly = true,
+        ),
+    ),
+    maxStepsPerEpisode = maxStepsPerEpisode,
+    successfulTermination = { it.done },
+    callbacks = listOf(
+        printEpisodeStart(10),
+        onEpisodeEnd { epsilonDecay() }
+    )
+)
+println("Starting training")
+val training = trainer.train(maxEpisodes(trainingEpisodes))
+mk.writeNPY(fileName, trainingQtable)
+
+val testingQtable = mk.readNPY<Double, D2>(fileName).asD2Array()
+val tester = episodicTrainer(
+    env = recordEnv,
+    agent = policyAgent(
+        id = "testing",
+        policy = policy.pi()
+    ),
+    maxStepsPerEpisode = maxStepsPerEpisode,
+    successfulTermination = { it.reward == 0.0 },
+    callbacks = listOf(
+        printEpisodeStart(10)
+    )
+)
+println("Starting testing")
+val test = tester.train(maxEpisodes(testEpisodes))
+println("Training average reward: ${training.totalAverageReward}")
+println("Test average reward: ${test.totalAverageReward}")
+
+val vTable = mk.d1array(25) { testingQtable[it].max() ?: 0.0 }
+
+println("QTable")
+printQTable(
+    testingQtable, rows = 5, columns = 5, actionSymbols = mapOf(
+        0 to "↑", 1 to "→", 2 to "↓", 3 to "←"
+    )
+)
+println("Target Policy")
+printPolicyGrid(
+    policy, 5, 5, mapOf(
+        0 to "↑", 1 to "→", 2 to "↓", 3 to "←"
+    )
+)
+displayVideos(recordEnv.folder)
+val plot1 = plotPolicyActionValueGrid(
+    testingQtable, 5, 5, mapOf(
+        0 to "↑", 1 to "→", 2 to "↓", 3 to "←"
+    )
+)
+saveBufferedImageAsPng(plot1.toBufferedImage(), File(recordEnv.folder, "qtable_grid.png"))
+val plot2 = plotPolicyStateValueGrid(
+    policy, vTable, 5, 5, mapOf(
+        0 to "↑", 1 to "→", 2 to "↓", 3 to "←"
+    )
+)
+saveBufferedImageAsPng(plot2.toBufferedImage(), File(recordEnv.folder, "policy_grid.png"))
---
+++ examples/monte_carlo_control/MazeOnPolicyMonteCarloControl.main.kts
@@ -0,0 +1,102 @@
+#!/usr/bin/env kotlin -Djava.awt.headless=false -Dprism.order=sw
+
+@file:Repository("https://repo1.maven.org/maven2")
+@file:Repository("https://central.sonatype.com/repository/maven-snapshots/")
+
+@file:DependsOn("io.github.kotlinrl:integration:0.1.0-SNAPSHOT")
+@file:DependsOn("io.github.kotlinrl:tabular:0.1.0-SNAPSHOT")
+@file:DependsOn("io.github.kotlinrl:envs:0.1.0-SNAPSHOT")
+@file:DependsOn("io.github.kotlinrl:rendering:0.1.0-SNAPSHOT")
+
+import io.github.kotlinrl.core.*
+import io.github.kotlinrl.core.RecordVideo
+import io.github.kotlinrl.core.api.*
+import io.github.kotlinrl.core.wrapper.*
+import io.github.kotlinrl.envs.*
+import io.github.kotlinrl.rendering.*
+import io.github.kotlinrl.tabular.*
+import io.github.kotlinrl.tabular.mc.*
+import org.jetbrains.kotlinx.kandy.letsplot.export.*
+import org.jetbrains.kotlinx.multik.api.*
+import org.jetbrains.kotlinx.multik.api.io.*
+import org.jetbrains.kotlinx.multik.ndarray.data.*
+import java.io.*
+
+
+val maxStepsPerEpisode = 1_000
+val trainingEpisodes = 1_000
+val testEpisodes = 30
+val initialEpsilon = 0.8
+val linearDecayRate = 0.00089
+val minEpsilon = 0.0
+val gamma = 0.99
+val fileName = "MazeOnPolicyMonteCarloControl.npy"
+
+val env = Maze(render = true)
+val recordEnv = RecordVideo(env = env, folder = "./videos/maze_on_policy_mcc", 10)
+val trainingQtable = mk.d2array(25, 4) { 0.0 }
+val (epsilonSchedule, epsilonDecay) = ParameterSchedule.linearDecay(
+    initialValue = initialEpsilon,
+    minValue = minEpsilon,
+    decayRate = linearDecayRate,
+    callback = { episode, parameter ->
+        if (episode % 100 == 0) {
+            println("Episode: $episode, Epsilon: $parameter")
+        }
+    }
+)
+
+val trainer = episodicTrainer(
+    env = env,
+    agent = learningAgent(
+        id = "training",
+        algorithm = OnPolicyMonteCarloControl(
+            epsilon = epsilonSchedule,
+            Q = trainingQtable,
+            gamma = gamma,
+            firstVisitOnly = true,
+        ),
+    ),
+    maxStepsPerEpisode = maxStepsPerEpisode,
+    successfulTermination = { it.done },
+    callbacks = listOf(
+        printEpisodeStart(10),
+        onEpisodeEnd { epsilonDecay() }
+    )
+)
+println("Starting training")
+val training = trainer.train(maxEpisodes(trainingEpisodes))
+mk.writeNPY(fileName, trainingQtable)
+
+val testingQtable = mk.readNPY<Double, D2>(fileName).asD2Array()
+val tester = episodicTrainer(
+    env = recordEnv,
+    agent = policyAgent(
+        id = "testing",
+        policy = testingQtable.greedy()
+    ),
+    maxStepsPerEpisode = maxStepsPerEpisode,
+    successfulTermination = { it.reward == 0.0 },
+    callbacks = listOf(
+        printEpisodeStart(10)
+    )
+)
+println("Starting testing")
+val test = tester.train(maxEpisodes(testEpisodes))
+println("Training average reward: ${training.totalAverageReward}")
+println("Test average reward: ${test.totalAverageReward}")
+
+printQTable(
+    testingQtable, rows = 5, columns = 5, actionSymbols = mapOf(
+        0 to "↑", 1 to "→", 2 to "↓", 3 to "←"
+    )
+)
+displayVideos(recordEnv.folder)
+val plot = plotPolicyActionValueGrid(
+    testingQtable, 5, 5, mapOf(
+        0 to "↑", 1 to "→", 2 to "↓", 3 to "←"
+    )
+)
+saveBufferedImageAsPng(plot.toBufferedImage(), File(recordEnv.folder, "policy_grid.png"))
+
+
